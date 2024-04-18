7abdd28b0c52dd0d83cd990a1a463527e30184b8
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-04-18 17:47:15 -0300

    Pipeline en un entorno de pruebas

--- app.test.js
+++ app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
-const app = require('./app'); 
+const app = require('./app');
 
 describe('GET /', () => {
+ let server;
+
+ beforeAll(() => {
+    server = app.startServer(3003); // Iniciar el servidor antes de los tests
+ });
+
+ afterAll(done => {
+    server.close(done); // Detener el servidor después de los tests
+ });
+
  it("Enviando respuesta desde node", async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Enviando respuesta desde node');
  });
